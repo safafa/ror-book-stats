3a275a51a3be97c57013f0e094d15b00593be5a5
safafa <safaaballagh97@gmail.com>
2021-06-23 16:51:58 +0100

    Fix errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[login password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[username email password_confirmation current_password])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
-  attr_accessor :login 
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true
 
-  def self.find_for_authentication warden_condition
-    conditions = warden_condition.dup 
+  def self.find_for_authentication(warden_condition)
+    conditions = warden_condition.dup
     login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+    where(conditions).where(['lower(username) = :value OR lower(email) = :value',
+                             { value: login.strip.downcase }]).first
   end
 end
