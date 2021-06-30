09e800248aef280236c7192ab214ce4a5bc4585e
safafa <safaaballagh97@gmail.com>
2021-06-30 12:30:27 +0100

    Fix restfull routes and image bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-  resources :books do
+  resources :users, only:[:show]
+  resources :books, only:[:show,:index,:new,:create,:edit,:update,:destroy] do
     get :external, on: :collection
   end
-  resources :groups
+  resources :groups, only:[:show,:index,:new,:create,:edit,:update,:destroy]
   
   root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
