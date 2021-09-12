41ab95b8bc4d9db14245f053cde97fe263afaea1
safafa <safaaballagh97@gmail.com>
2021-09-12 20:29:20 +0100

    Fix rubocop errors

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-    default from: 'aballaghsafa@gmail.com'
+  default from: 'aballaghsafa@gmail.com'
 
-    def welcome_email
-        @user = params[:user]
-        @url  = 'https://fierce-ocean-28088.herokuapp.com/'
-        mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-    end
+  def welcome_email
+    @user = params[:user]
+    @url = 'https://fierce-ocean-28088.herokuapp.com/'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
